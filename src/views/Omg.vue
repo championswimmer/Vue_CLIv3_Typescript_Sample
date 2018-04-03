c434179a23666ec091bd6bfa18f0ffdc18d8a47d
Arnav Gupta <arnav@codingblocks.com>
2018-04-03 17:50:13 +0530

    basic ts app yo
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/views/Omg.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>OMG</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+export default class Omg extends Vue {}
+</script>
+
+<style>
+
+</style>
+
